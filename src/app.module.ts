17ad15505c6ce2cc9dc054d583e4d6a46e3399d2
Pabel Hernández <pabel.hernandez@live.com>
2023-08-15 04:46:30 -0600

    update user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '',
+    database: 'mega_bib',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'], // Ruta de las entidades
+    synchronize: true,
+  }),AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
