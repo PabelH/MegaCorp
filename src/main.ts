9b93c58e6084ac40cdab3a52bb76e621ec23e6f7
Pabel Hernandez <pabel.hernandez@live.com>
2023-08-24 15:26:46 -0600

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { config } from 'dotenv';
+config();
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
 
   //Swagger
