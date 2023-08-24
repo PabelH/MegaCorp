269b29aaa59d7db183d37802cf998f79b3c2f0c9
Pabel Hernandez <pabel.hernandez@live.com>
2023-08-24 14:38:05 -0600

    setup prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { BookModule } from './book/book.module';
 import { AuthModule } from './auth/auth.module';
+import { databaseConfig } from './config';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '',
-    database: 'mega_bib',
+    // host: 'localhost',
+    // port: 3306,
+    // username: 'root',
+    // password: '',
+    // database: 'mega_bib',
+    host: databaseConfig.host,
+    port: databaseConfig.port,
+    username: databaseConfig.username,
+    password: databaseConfig.password,
+    database: databaseConfig.database,
     entities: [__dirname + '/**/*.entity{.ts,.js}'], // Ruta de las entidades
     synchronize: true,
   }), UserModule, BookModule, AuthModule],
---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const databaseConfig = {
+  type: process.env.DB_TYPE || 'mysql',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
+  username: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_DATABASE || 'mega_bib',
+};
