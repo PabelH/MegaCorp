bfe0983c332744c22b653128d29b7434e551c76c
Pabel Hernández <pabel.hernandez@live.com>
2023-08-24 19:08:15 -0600

    update

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const databaseConfig = {
   type: process.env.DB_TYPE || 'mysql',
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT) || 3306,
+  port: +process.env.DB_PORT || 3306,
   username: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_DATABASE || 'mega_bib',
